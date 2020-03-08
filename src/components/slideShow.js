2c07d6f02f89618b6adebeb5384b6f8572fc4cad
abdifatah <abdifatahzammiir@gmail.com>
2020-03-08 22:46:48 +0300

    change title

--- src/components/slideShow.js
+++ src/components/slideShow.js
@@ -12,13 +12,18 @@ const SlideShow = ({ mainTitle, subTitle }) => {
 					<Carousel.Item>
 						<div id="slider16" className="row rev_slider fullwidthbanner">
 							<div className="col-lg-6 col-md-6 col-sm-6 heading-con">
-								<div className="heading-one color-dark mt-3">{mainTitle} </div>
-								<div className="heading-two color-dark mt-3 mb-4">
-									{subTitle}
+								<div className="heading-one color-dark mt-3 mb-4">
+									{subTitle}{" "}
 								</div>
-								<a className="btn btn-l btn-default animated-btn" href="/">
+								{/* <div className="heading-two color-dark mt-3 mb-4">
+									{subTitle}
+								</div> */}
+								<Link
+									className="btn btn-l btn-default animated-btn"
+									to="/about"
+								>
 									Learn More
-								</a>
+								</Link>
 							</div>
 							<div className="col-md-6 col-lg-6 col-sm-6 r-pic">
 								<img className="size-pic" src={Concept21} alt="" />
@@ -28,10 +33,12 @@ const SlideShow = ({ mainTitle, subTitle }) => {
 					<Carousel.Item>
 						<div id="slider16" className="row rev_slider fullwidthbanner">
 							<div className="col-lg-6 col-md-6 col-sm-6 heading-con">
-								<div className="heading-one color-dark mt-3">{mainTitle} </div>
-								<div className="heading-two color-dark mt-3 mb-4">
-									{subTitle}
+								<div className="heading-one color-dark mt- mb-4">
+									{mainTitle}{" "}
 								</div>
+								{/* <div className="heading-two color-dark mt-3 mb-4">
+									{subTitle}
+								</div> */}
 								<Link
 									className="btn btn-l btn-default animated-btn"
 									to="/about"
