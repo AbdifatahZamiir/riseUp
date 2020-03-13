eeee535961b19deaf780f359142f84c84e49dc13
abdifatah <abdifatahzammiir@gmail.com>
2020-03-13 10:06:46 +0300

    fixed some position error in latest event

--- src/components/latestEvent.js
+++ src/components/latestEvent.js
@@ -46,55 +46,48 @@ const LatestEvent = () => {
 					<span className="title-underline">Latest Events </span>
 				</h3>
 				<div className="row">
-					<div className="blog grid grid-view">
-						<div className="row isotope">
-							{allPosts.allMarkdownRemark.edges.map(({ node }) => {
-								const { id } = node;
-								const { slug } = node.fields;
-								const { featuredImg, title, publishedDate } = node.frontmatter;
-								return (
-									<div
-										className="item post grid-sizer col-md-4 col-lg-4"
-										key={id}
-									>
-										<figure className="overlay overlay1 rounded mb-3">
-											<Link to={slug}>
-												{!!featuredImg && !!featuredImg.childImageSharp ? (
-													<Img
-														style={{ width: "100%", height: "300px" }}
-														fluid={featuredImg.childImageSharp.fluid}
-														alt={title}
-													/>
-												) : (
-													<img
-														style={{ width: "100%", height: "300px" }}
-														src={featuredImg.publicURL}
-														alt={title}
-													/>
-												)}
-											</Link>
-											<figcaption>
-												<h5 className="from-top mb-0">Read More</h5>
-											</figcaption>
-										</figure>
-										<h2 className="post-title">
-											<Link style={{ boxShadow: `none` }} to={slug}>
-												{title}
-											</Link>
-										</h2>
+					{allPosts.allMarkdownRemark.edges.map(({ node }) => {
+						const { id } = node;
+						const { slug } = node.fields;
+						const { featuredImg, title, publishedDate } = node.frontmatter;
+						return (
+							<div className="item post grid-sizer col-md-4 col-lg-4" key={id}>
+								<figure className="overlay overlay1 rounded mb-3">
+									<Link to={slug}>
+										{!!featuredImg && !!featuredImg.childImageSharp ? (
+											<Img
+												style={{ width: "100%", height: "300px" }}
+												fluid={featuredImg.childImageSharp.fluid}
+												alt={title}
+											/>
+										) : (
+											<img
+												style={{ width: "100%", height: "300px" }}
+												src={featuredImg.publicURL}
+												alt={title}
+											/>
+										)}
+									</Link>
+									<figcaption>
+										<h5 className="from-top mb-0">Read More</h5>
+									</figcaption>
+								</figure>
+								<h2 className="post-title">
+									<Link style={{ boxShadow: `none` }} to={slug}>
+										{title}
+									</Link>
+								</h2>
 
-										<div className="meta mb-0">
-											<span className="date">
-												<FaClock size="19px" color="d8681d" className="mb-2" />
-												{"     "}
-												{publishedDate}
-											</span>
-										</div>
-									</div>
-								);
-							})}
-						</div>
-					</div>
+								<div className="meta mb-0">
+									<span className="date">
+										<FaClock size="19px" color="d8681d" className="mb-2" />
+										{"     "}
+										{publishedDate}
+									</span>
+								</div>
+							</div>
+						);
+					})}
 				</div>
 			</div>
 		</div>
